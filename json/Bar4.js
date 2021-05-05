f37bf9af5c70554ab50c90828bf8827c3c7d9412
Mandy Farell <Mandy13ib@gmail.com>
2021-05-04 20:20:53 -0400

    adding project two to new repo

---
+++ json/Bar4.js
@@ -0,0 +1,9 @@
+// Define a function that will create charts for given sample
+function buildCharts(selection) {
+  
+    // Read the json data
+    d3.json('json/averages.json').then((data) => {
+       console.log(data) // this is the data from the flask 
+
+    })
+}
